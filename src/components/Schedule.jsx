c40897ad2619fd8f656feb4158cb4661cf5330bf
imzero9 <skdud000322@naver.com>
2023-09-14 18:00:30 +0900

    💄 Style: 개요 모바일용 텍스트 스타일 적용

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -10,8 +10,8 @@ export default function Schedule() {
   const { isMobile } = useResizeContext();
   return (
     <Layout>
-      <Title>제주 온보딩 워케이션 일정</Title>
-      <SubTitle>3박 4일간 진행됩니다.</SubTitle>
+      <Title isMobile={isMobile}>제주 온보딩 워케이션 일정</Title>
+      <SubTitle isMobile={isMobile}>3박 4일간 진행됩니다.</SubTitle>
       {!isMobile && <Table src={process.env.PUBLIC_URL + '/img/s3-table.svg'}></Table>}
       {isMobile && (
         <>
