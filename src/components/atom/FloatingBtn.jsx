ba9327f0fe791208ce6a6fad01d71ba3beb4f1a9
min-bok <leehy0782@gmail.com>
2023-09-15 08:50:35 +0900

    💄Style: FloatingBtn 레이아웃 스타일링

--- src/components/atom/FloatingBtn.jsx
+++ src/components/atom/FloatingBtn.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 export default function FloatingBtn() {
+  const [isOpen, setIsOpen] = useState(false);
   const handleButtonClick = () => {
     // 버튼이 클릭될 때 수행할 작업 정의
   };
 
   return (
     <Wrap>
-      <button className="shareBtn" onClick={handleButtonClick}>
-        버튼 내용
-      </button>
-      <button className="topBtn" onClick={handleButtonClick}>
-        버튼 내용
-      </button>
+      <div className="expand">
+        <div>
+          <button className="linkBtn"></button>
+          <hr />
+          <button className="kakaoBtn"></button>
+        </div>
+      </div>
+      <button className="circle shareBtn" onClick={handleButtonClick}></button>
+      <button className="circle topBtn" onClick={handleButtonClick}></button>
     </Wrap>
   );
 }
 
 const Wrap = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   position: fixed;
@@ -26,15 +31,67 @@ const Wrap = styled.div`
   gap: 21px;
   align-self: flex-end;
 
-  background-color: pink;
-  button.shareBtn {
-    /* right: 0; */
-    background-color: #007bff; /* 배경색 설정 */
-    color: #fff; /* 글자색 설정 */
-    padding: 10px 20px; /* 패딩 설정 */
+  div.expand {
+    position: absolute;
+    width: 3.75rem;
+    height: 180px;
+    background-color: var(--dark-color);
+    bottom: 5.2rem;
+    border-radius: 40px;
+
+    div {
+      display: flex;
+      height: 5.063rem;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+      margin-top: 1.5rem;
+      padding: 0 0.688rem;
+
+      hr {
+        width: 100%;
+        border: none;
+        height: 2px;
+        background-color: var(--light-color);
+        border-radius: 1px; /* 둥근 끝 모양을 만들기 위한 반지름 값 */
+      }
+    }
+
+    button {
+      width: 2.344rem;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+      background-color: transparent;
+    }
+
+    button.linkBtn {
+      height: 1.688rem;
+      background-image: url(${process.env.PUBLIC_URL + '/img/linkIcon.svg'});
+    }
+
+    button.kakaoBtn {
+      height: 1.563rem;
+      background-image: url(${process.env.PUBLIC_URL + '/img/kakaoIcon.svg'});
+    }
+  }
+
+  button.circle {
+    width: 3.75rem;
+    height: 3.75rem;
+    border-radius: 50%;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
     border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    /* 필요한 다른 스타일링 추가 */
+    z-index: 999;
+  }
+
+  button.shareBtn {
+    background-color: var(--main-color);
+    background-image: url(${process.env.PUBLIC_URL + '/img/shareIcon.svg'});
+  }
+
+  button.topBtn {
+    background-color: #d9d9d9;
+    background-image: url(${process.env.PUBLIC_URL + '/img/topIcon.svg'});
   }
 `;
